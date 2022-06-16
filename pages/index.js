f864265412ed42ca8d48642464d4b4b369004559
AbbeyBa <abbey@krisullis.com>
2022-06-17 01:23:41 +0530

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Krisullis <a href="https://nextjs.org">Next.js!</a>
+          Krisullis 
         </h1>
       
         
