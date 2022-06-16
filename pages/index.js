80fef7037117f2dac7fbabcbfa8fa84ba980bb29
AbbeyBa <abbey@krisullis.com>
2022-06-17 00:26:46 +0530

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Krisullis <a href="https://nextjs.org">Next.js!</a>
         </h1>
-        <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/721178908?h=f76b8426ea&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Krisullis Butterflies"></iframe></div>
+        <div style={{padding:56.25% 0 0 0;position:relative;}}><iframe src="https://player.vimeo.com/video/721178908?h=f76b8426ea&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Krisullis Butterflies"></iframe></div>
         <script src="https://player.vimeo.com/api/player.js"></script>
         
 
