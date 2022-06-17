56d05d1512eab8d0fda33481bcef7eb313b8f801
AbbeyBa <abbey@krisullis.com>
2022-06-17 09:11:36 +0530

    1 file modified

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,7 @@
+import styles from '../styles/Home.module.css';
 
 
 
-const About = () => <h1>About page!</h1>
+const About = () => <h1 className={styles.title}>
+  About page!</h1>
 export default About;
