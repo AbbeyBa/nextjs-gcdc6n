5dfe3e35e78bda19f3dbee38619755cbec2473a8
AbbeyBa <abbey@krisullis.com>
2022-06-17 20:30:35 +0530

    2 files modified

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,17 @@
 import styles from '../styles/Home.module.css';
-import ReactPlayer from 'react-player';
+import ReactPlayer from 'react-player/lazy';
+import { useState } from 'react';
 
 
 
- 
 
- const About = () => (
-     <div className={ styles.home }>
-         <ReactPlayer url="https://vimeo.com/721178908" />
+
+export default function VideoPlayer(){
+  const [isPlaying, setIsPlaying] = useState(true);
+
+  return (
+     <div className={styles.playerwrapper}>
+         <ReactPlayer url="https://vimeo.com/721178908" playing={ isPlaying } />
      </div>
- )
-export default About; 
+  )
+ }
