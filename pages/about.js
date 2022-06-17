55ea5b698e6207decd2c173da85a542fd7efda5b
AbbeyBa <abbey@krisullis.com>
2022-06-17 18:47:34 +0530

    1 file modified

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,13 @@
 import styles from '../styles/Home.module.css';
+import ReactPlayer from 'react-player';
 
 
 
-const About = () => <h1 className={styles.title}>
-  About page!</h1>
  
 
+ const About = () => (
+     <div className={ styles.home }>
+         <ReactPlayer url="https://vimeo.com/721178908" />
+     </div>
+ )
 export default About; 
